958cc07af5a833e5d2e5fb3e4741518927f91bbb
adilavirk <adilavirk0@gmail.com>
2024-01-01 07:55:01 +0500

    learnt about server-side-api-call

---
+++ 02project/app/ServerSideApiCall/page.jsx
@@ -0,0 +1,53 @@
+// import React, { useEffect, useState } from 'react'
+const fetchProducts = async () => {
+    const response = await fetch('https://fakestoreapi.com/products')
+    let items = await response.json();
+    return items;
+    // console.log(items)
+    // setProducts(items);
+    // setLoader(false);
+}
+const Products = async () => {
+    // const [products, setProducts] = useState([]);
+    // const [loader, setLoader] = useState(true)
+    const products = await fetchProducts();
+    const loader = false;
+
+
+    // useEffect(() => {
+    //     fetchProducts();
+    // }, [products])
+
+    return (
+
+        <>
+            {products.length ?
+
+                <table>
+                    <tr>
+                        <th>image</th>
+                        <th>id</th>
+                        <th>title</th>
+                        <th>price</th>
+                        <th>category</th>
+                        <th>description</th>
+
+                    </tr>
+                    {products.map((item) => (
+                        <tr key={item.id}>
+                            <td><img src={item.image} alt={`${item.title}'s image`} width={90} /></td>
+                            <td>{item.id}</td>
+                            <td>{item.title}</td>
+                            <td>{item.price}</td>
+                            <td>{item.category}</td>
+                            <td>{item.description}</td>
+                        </tr>
+                    ))
+                    }
+                </table> : loader ? <h1 className='font-bold text-4 1xl flex justify-center'>Loading...</h1> : <h1>Products Not Found!</h1>}
+            {/* <button onClick={fetchProducts}>Get Products</button> */}
+        </>
+    )
+}
+
+export default Products
